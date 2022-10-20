5fd87d3658431c08abc2e07a517baf0ee5b4ab07
ellipse120 <ellipse120@gmail.com>
2022-10-20 10:51:47 +0800

    feat(UI): add `quasar`

--- src/views/business-components/environment-inspection.vue
+++ src/views/business-components/environment-inspection.vue
@@ -4,6 +4,8 @@
 
 <template>
   <div>
+    <q-btn label="Do something" />
+    <q-icon name="alarm" />
     环境检查
   </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,20 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import WindiCSS from "vite-plugin-windicss";
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), WindiCSS()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    vueJsx(),
+    WindiCSS(),
+    quasar({
+      sassVariables: 'src/assets/quasar-variables.sass'
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
